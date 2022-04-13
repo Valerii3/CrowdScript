9dd6d3a9d99ec852e215d297c69b26ba5ddb729f
Valerii3 <vallerus55@gmail.com>
2022-04-13 22:11:34 +0300

    realized std functions

---
+++ functions.h
@@ -0,0 +1,32 @@
+#ifndef CROWD_H
+#define CROWD_H
+
+#include <stdlib.h>
+#include <algorithm>
+#include <cassert>
+#include <cmath>
+#include <cstddef>
+#include <deque>
+#include <iomanip>
+#include <iostream>
+#include <map>
+#include <set>
+#include <sstream>
+#include <string>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+#include <cstring>
+#include <fstream>
+#include <chrono>
+#include <random>
+#include <queue>
+
+
+int gcd(int a, int b);
+
+int max(int a, int b);
+
+int min(int a, int b);
+
+#endif //CROWD_CROWD_H
