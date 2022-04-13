d072ce541a43edfc9085df38d6b2f08ec1b177f8
Valerii3 <vallerus55@gmail.com>
2022-04-13 22:07:41 +0300

    CrowdScript functions

---
+++ Crowd.h
@@ -0,0 +1,27 @@
+#ifndef CROWD_H
+#define CROWD_H
+
+
+function crowd_gcd(arg1: int, arg2: int) -> int{
+    if (b == 0){
+        return a;
+    }
+    return gcd(b, a % b);
+}
+
+
+function crowd_max(arg1: int, arg2: int) -> int{
+    if (arg1 > arg2){
+        return ag1;
+    }
+    return agr2;
+}
+
+
+function crowd_min(arg1: int, arg2: int) -> int{
+    if (arg1 > arg2){
+        return arg2;
+    }
+    return agr1;
+}
+#endif //CROWD_CROWD_H
